af33a398500291ceabc09c2290b9869a3ef67fe3
Abdur Rehman <abdurrehmanmughal1@gmail.com>
2023-07-18 12:38:25 +0500

    feat: sum of two numbers

---
+++ twoSum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twoSum
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] arr = {
+            10,
+            20
+
+        };
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum = sum + arr[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
